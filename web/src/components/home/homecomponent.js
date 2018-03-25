453fcb761491535e6b0a86113be8bf398d3a3b82
lujianjun <812518099@qq.com>
2018-03-25 13:16:58 +0800

    项目搭建

--- web/src/components/home/homecomponent.js
+++ web/src/components/home/homecomponent.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
 import HeaderComponent from '../header/headercomponent'
+import './home.scss'
 
 export default class HomeComponent extends React.Component{
     render(){
         return (
-            <div>
+            <div className="home">
                 <HeaderComponent></HeaderComponent>
-                
+                <div className="home_body"></div>
+                <div className="home_footer"></div>
             </div>
         )
     }
--- web/src/components/list/listcomponent.js
+++ web/src/components/list/listcomponent.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+import HeaderComponent from '../header/headercomponent'
+import './list.scss'
+
 export default class ListComponent extends React.Component{
     render(){
         return (
-            <div>11</div>
+            <div className="list">
+                <HeaderComponent></HeaderComponent>
+                <div className="list_body">
+                    <div className="list_banner"></div>
+                    <div className="list_main">
+                        
+                    </div>
+                </div>
+            </div>
         )
     }
 }
